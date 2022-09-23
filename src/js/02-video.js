22a1de7805292910d276e56d4bbfb75819755974
dmstepanuik <0669218646@ukr.net>
2022-09-23 21:13:16 +0300

    task 08- 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const localStorageKey = 'videoplayer - current - time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(loadPosition());
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+  savePosition(e.seconds);
+}
+
+function savePosition(value) {
+  localStorage.setItem(localStorageKey, String(value));
+}
+
+function loadPosition() {
+  return localStorage.getItem(localStorageKey) || 0;
+}
